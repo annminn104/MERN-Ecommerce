f18a19d4288753adeb676ee68776549a22bbdf1e
annminn104 <annminn104@gmail.com>
2021-07-24 23:06:38 +0700

    make it better

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,4 +1,11 @@
 const tailwindcss = require("tailwindcss");
 module.exports = {
-  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
+  plugins: [
+    tailwindcss("./tailwind.js"),
+    require("autoprefixer"),
+    require("@fullhuman/postcss-purgecss")({
+      content: ["./src/**/*.js", "./public/index.html"],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+  ],
 };
